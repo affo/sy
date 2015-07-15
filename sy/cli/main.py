ef1416dc5474e5aa92c10559970875c5ee58e41d
Lorenzo Affetti <affox90@gmail.com>
2015-07-15 09:28:52 +0200

    using subparsers for subcommand

--- sy/cli/main.py
+++ sy/cli/main.py
@@ -1,8 +1,24 @@
 import argparse
-from sy.cli import rmq
 
 def main():
-    parser = argparse.ArgumentParser(description='Sy client to have a look at monitored data.')
-    parser.add_argument('-t', dest='topic', default='sensors.*',
-        help='The topic to subscribe to')
+    parser = argparse.ArgumentParser(
+        description=
+            """
+            Client that performs requests to Sy's Daemon.
+            It allows to create, remove, list sensors and get data published by them.
+            """
+    )
+
+    subparsers = parser.add_subparsers()
+    parser_add = subparsers.add_parser('add', help='Create a new sensor')
+    parser_del = subparsers.add_parser('remove', help='Remove an existing sensor')
+    parser_list = subparsers.add_parser('list', help='List active sensors')
+    parser_stypes = subparsers.add_parser('types', help='List available sensor types')
+
+    parser_add.add_argument('cid', help='The container\'s id')
+    parser_add.add_argument('stype', help='The sensor type')
+    parser_del.add_argument('cid', help='The container\'s id')
+    parser_del.add_argument('stype', help='The sensor type')
+    #parser.add_argument('-t', dest='topic', default='sensors.*',
+    #    help='The topic to subscribe to')
     args = parser.parse_args()
